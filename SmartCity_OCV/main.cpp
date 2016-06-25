1554a0e43118f8686a13bb4fc4f4323978cb440b
Guilhem <AZZANO guilhem>
2016-06-25 12:24:57 +0200

    Code de base pour afficher une image

--- SmartCity_OCV/main.cpp
+++ SmartCity_OCV/main.cpp
@@ -1,10 +1,53 @@
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
+#include <stdlib.h>
 
-int main()
+#include <opencv/highgui.h>
+
+
+int main (int argc, char* argv[])
+
 {
-    cout << "Hello World!" << endl;
-    return 0;
-}
 
+  IplImage* img = NULL;
+
+  const char* window_title = "Hello, OpenCV!";
+
+
+  if (argc < 2)
+
+  {
+
+    fprintf (stderr, "usage: %s IMAGE\n", argv[0]);
+
+    return EXIT_FAILURE;
+
+  }
+
+  img = cvLoadImage(argv[1], CV_LOAD_IMAGE_UNCHANGED);
+
+
+  if (img == NULL)
+
+  {
+
+    fprintf (stderr, "couldn't open image file: %s\n", argv[1]);
+
+    return EXIT_FAILURE;
+
+  }
+
+  cvNamedWindow (window_title, CV_WINDOW_AUTOSIZE);
+
+  cvShowImage (window_title, img);
+
+  cvWaitKey(0);
+
+  cvDestroyAllWindows();
+
+  cvReleaseImage(&img);
+
+
+  return EXIT_SUCCESS;
+
+}
